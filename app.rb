1469a5e14121063761edcfd69f32744099666cfc
bonbon <nepunepubon@gmail.com>
2021-01-27 20:12:10 +0800

    added case detection logic?

--- app.rb
+++ app.rb
@@ -7,8 +7,16 @@ def cypher (phrase, shift)
     phrase_to_array = phrase.chars
 
     #the actual logic
-    phrase_to_array.map { |char| letters.include?(char) ? letters[(letters.find_index(char)) + shift - letters.length] : char }.join
+    phrase_to_array.map do |char| 
+        if char == char.upcase
+          letters.include?(char.downcase) ? letters[(letters.find_index(char.downcase)) + shift - letters.length].upcase : char
+        else
+          letters.include?(char) ? letters[(letters.find_index(char)) + shift - letters.length] : char 
+        end
+    end.join
 end
+
+
 
 
 # for each character in the letters array, there will be an assigned index
@@ -24,5 +32,6 @@ end
 # ok that works
 
 # now figure out cases
+# if what is being passed in the map is an upcase, do upcase return. if not then not
 
-print cypher("zygote", 5)
+print cypher("Zygotes are cells I think", 2)
